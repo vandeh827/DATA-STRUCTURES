a2a664200af50e7acb657350c8ae025725d34c88
vandeh827 <130293990+vandeh827@users.noreply.github.com>
2024-05-28 16:03:30 +0100

    Algorithm

---
+++ Algorithm.java
@@ -0,0 +1,10 @@
+//
+// algorithm
+//
+//Get the word
+//Convert the word to lowercase
+// create method to check if word has similar characters
+//check if it is the same when reversed
+//Count the number of characters that are the same
+// test word
+//Print number result
